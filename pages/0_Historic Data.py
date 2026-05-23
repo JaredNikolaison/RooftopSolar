7de8a3f318e36f17363b2c761adc5b4191d42743
JaredNikolaison <81340957+JaredNikolaison@users.noreply.github.com>
2026-05-24 08:23:05 +1200

    various app  alterations

---
+++ pages/0_Historic Data.py
@@ -0,0 +1,164 @@
+from os import write
+
+import streamlit as st
+import pandas as pd
+
+import altair as alt
+
+
+# set up pages
+
+st.set_page_config(
+    page_title="Historic Data",
+    layout="wide",
+
+)
+
+
+# st.write("""s
+# # My first app
+# Hello *world!*
+# """)
+# df = pd.read_excel("wine_data.xls")
+#
+# st.line_chart(df)
+
+
+st.title("Historic Data")
+
+
+
+solarcapacity_df  = pd.read_csv("Datafiles/solarcapacity.csv")
+demand_df = pd.read_csv("Datafiles/Demand_Clean.csv")
+pricing_df  = pd.read_csv("Datafiles/Wholesale_Pricing.csv")
+
+
+capacity_tab, generation_tab, demand_tab, pricing_tab = st.tabs(["Solar Capacity", "Solar Generation", "Demand and Load", "Pricing"])
+
+
+with capacity_tab.container():
+    # st.subheader("Solar Capacity Over Time")
+    # st.line_chart(
+    #     solarcapacity_df,
+    #     x="date",
+    #     y="adjustedcapacity",
+    #     color="blue",
+    #     y_label="Capacity (MW)",
+    #     x_label="Month",
+    #
+    # )
+
+
+    capacity_graph = alt.Chart(solarcapacity_df).mark_line(color = 'blue').encode(
+        x=alt.X('date:T', axis=alt.Axis(format='%Y', title='Date', tickCount="year"),
+                ),
+        y=alt.Y('adjustedcapacity', axis=alt.Axis(title='Capacity (MW)')).scale(zero=False),
+       tooltip= [alt.Tooltip('date:T', title = 'Date', format = '%B-%Y'),
+                 alt.Tooltip('generation_gwh', title = 'Capacity (MW)')]
+
+    ).interactive().properties(title="Solar Generation Over Time")
+    st.altair_chart(capacity_graph, use_container_width=True)
+
+
+
+
+    st.write("The data for New Zealand's roof top solar capacity is provided form August 2013 to December 2025 by the NZ Electricity Authority."
+             " Performance decay / degradation has been taken as an average value of 0.5% per year. "
+             )
+
+with generation_tab.container():
+    # st.subheader("Solar Generation Over Time")
+    #
+    # st.line_chart(
+    #     solarcapacity_df,
+    #     x="date",
+    #     y="generation_gwh",
+    #     color="orange",
+    #
+    #     y_label="Generation (GWh)",
+    #     x_label="Month",
+    #
+    # )
+
+
+    generation_graph = alt.Chart(solarcapacity_df).mark_line(color = 'Red').encode(
+        x=alt.X('date:T', axis=alt.Axis(format='%Y', title='Date', tickCount="year"),
+                ),
+        y=alt.Y('generation_gwh', axis=alt.Axis(title='Generation (GWh)')).scale(zero=False),
+       tooltip= [alt.Tooltip('date:T', title = 'Date', format = '%B-%Y'),
+                 alt.Tooltip('generation_gwh', title = 'Generation (GWh)')]
+
+    ).interactive().properties(title="Solar Generation Over Time")
+    st.altair_chart(generation_graph, use_container_width=True)
+
+
+    st.write("Power data provided from NASA shows the average daily irradiation values which was then used to calculate solar generation using the formula: Generation(GWh) = Capacity(MW) × HoursinMonth × Capacityfactor / 1000 ")
+
+
+
+
+
+with demand_tab.container():
+    # st.subheader("Demand Over Time")
+    # st.line_chart(
+    #     demand_df,
+    #     x="date",
+    #     y="demand_gwh",
+    #     color="green",
+    #     y_label="Demand (GWh)",
+    #     x_label="Month",
+    # )
+
+    demand_df.rename(columns={'demand_gwh': 'Total Demand', 'net_grid_demand_gwh':'Net Grid Demand'}, inplace=True)
+
+    demand_chart = alt.Chart(demand_df).transform_fold(
+        fold=['Total Demand', 'Net Grid Demand'],
+        as_=['Total', 'Net']
+        ).mark_line().encode(
+            x = alt.X('date:T', axis=alt.Axis(format='%Y', title='Date', tickCount="year")),
+            y=alt.Y('Net:Q', axis=alt.Axis(title='Price ($/GWh)')).scale(zero=False),
+            color=alt.Color('Total:N', legend=alt.Legend(title="Legend")).scale(
+                  domain=['Total Demand', 'Net Grid Demand'],
+                  range=['Blue', 'Orange']
+
+
+            )
+
+        ).interactive().properties(
+        title="Total Electricity Demand vs Estimated Net Grid Demand"
+
+    )
+    st.altair_chart(demand_chart, use_container_width=True)
+
+    st.write("The total New Zealand power grid demand is provided form January 2013 to December 2025. This includes demand from all sources and is not filtered by industry."
+            "  \nNet Demand is calculated using the formula: NetDemand(GWh) = Demand(GWh) − Generation(GWh)")
+
+
+
+with pricing_tab.container():
+    # st.subheader("Pricing Over Time")
+    # st.line_chart(
+    #     pricing_df,
+    #     x="date",
+    #     y="price_GWh",
+    #     color="red",
+    #     y_label="Price ($/GWh)",
+    #     x_label="Month",
+    # )
+
+    pricing_graph = alt.Chart(pricing_df).mark_line(color = 'Green').encode(
+        x=alt.X('date:T', axis=alt.Axis(format='%Y', title='Date', tickCount="year"),
+                ),
+        y=alt.Y('price_GWh', axis=alt.Axis(title='Price ($/GWh)')).scale(zero=False),
+       tooltip= [alt.Tooltip('date:T', title = 'Date', format = '%B-%Y'),
+                 alt.Tooltip('price_GWh', title = 'Price ($/Gwh)')]
+
+    ).interactive().properties(title="Pricing Over Time")
+    st.altair_chart(pricing_graph, use_container_width=True)
+
+    st.write("The monthly wholesale pricing simple average data from the Electricity Authority is provided from January 2013 to December 2025.")
+
+
+
+
+
---
+++ pages/4_Solar Scenario Predictions.py
@@ -0,0 +1,217 @@
+from html.parser import charref
+import streamlit as st
+import pandas as pd
+import statsmodels.api as smf
+import altair as alt
+from streamlit import title
+
+# from urllib.parse import urljoin
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.neighbors import KNeighborsRegressor
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.model_selection import StratifiedKFold
+# from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+#
+# from sklearn.metrics import r2_score, mean_absolute_error, accuracy_score, mean_squared_error
+# from sklearn import metrics
+# from sklearn.cluster import KMeans
+# from sklearn import preprocessing
+
+# set up pages
+
+st.set_page_config(
+    page_title="Modelling",
+    layout="wide",
+
+
+)
+
+
+
+
+st.title("Solar Scenario Predictions")
+
+
+
+
+
+
+solar_multiplier = st.slider("Solar Muliplier", 0.1, 20.0, 1.0, step=0.05)
+net_demand_multiplier = st.slider("Net Demand Multiplier", 0.1, 20.0, 1.0, step=0.05)
+
+gdp = pd.read_csv("Datafiles/pricedemand.csv")
+
+model_solar_quad = smf.iolib.smpickle.load_pickle("Datafiles/model_solar_quad.pkl")
+
+
+def predictprice(net_demand_series, generation_series):
+    return model_solar_quad.predict(pd.DataFrame({"net_grid_demand_gwh": net_demand_series,
+                                                  "generation_gwh_y": generation_series}))
+
+
+gdp['predicted_price'] = predictprice(gdp['net_grid_demand_gwh'], gdp['generation_gwh_y'])
+
+gdp['predicted_price_modified'] = (predictprice(gdp['net_grid_demand_gwh'] * net_demand_multiplier, gdp['generation_gwh_y'] * solar_multiplier))
+gdp['Predicted Price'] = gdp['predicted_price_modified'].rolling(12).mean() # Predicted price smoothed - named Predicted Price to get around legend issues in graph legend
+gdp['Predicted Volatility'] = gdp['predicted_price_modified'].rolling(12).std() # Predicted volatility smoothed - named Predicted Volatility to get around legend issues in graph legend
+gdp["smoothed_price"] = gdp["price_GWh"].rolling(12).mean()
+gdp["Base Volatility"] = gdp["price_GWh"].rolling(12).std()
+
+price, volatility, price_impact = st.tabs(["Wholesale Electricity Price Predictions", "Volatility Predictions","Price Impact"])
+
+
+with price.container():
+
+
+    # solar_demand  = pd.read_csv("Datafiles/demand_solar_clean.csv")
+
+
+#     predicted_price_line = alt.Chart(gdp).mark_line().encode(
+#         x=alt.X('date:T', axis = alt.Axis(format = '%Y', title = 'Date',tickCount="year"),
+#         ),
+#         y=alt.Y('predicted_price_modified_smoothed', axis = alt.Axis(title = 'Price ($/GWh)')).scale(zero = False),
+#
+#         # color=alt.Color('predicted_price_label:N').scale(range=['red']).title('Legend').datum(''),
+#         color=alt.datum('Predicted Price'),
+#     ).interactive().properties(
+#         title=
+# "Wholesale Electricity Price (Smoothed)",
+#     )
+#
+#
+#     base_price_line = alt.Chart(gdp).mark_line(color = "blue").encode(
+#         x=alt.X('date:T', axis=alt.Axis(format='%Y', title='Date', tickCount="year"),
+#                 ),
+#         y=alt.Y('smoothed_price', axis=alt.Axis(title='Price ($/GWh)')).scale(zero=False),
+#         color = alt.datum('Base Price')
+#
+#     ).interactive()
+
+
+    gdp.rename(columns = {'smoothed_price':'Base Price'}, inplace = True)
+
+
+    # st.altair_chart(joined_chart, use_container_width=True)
+
+
+    price_chart = alt.Chart(gdp).transform_fold(
+    fold=['Predicted Price', 'Base Price'],
+    as_=['predicted', 'base']
+    ).mark_line().encode(
+        x = alt.X('date:T', axis=alt.Axis(format='%Y', title='Date', tickCount="year")),
+        y=alt.Y('base:Q', axis=alt.Axis(title='Price ($/GWh)')).scale(zero=False),
+        color=alt.Color('predicted:N', legend=alt.Legend(title="Legend")).scale(
+              domain=['Predicted Price', 'Base Price'],
+              range=['orange', 'blue']
+
+
+        )
+
+    ).interactive().properties(
+    title="Wholesale Electricity Price (Smoothed)"
+    )
+
+    st.altair_chart(price_chart, use_container_width=True)
+
+    st.write("This shows the base price compared against a predicted price calculated by the multipliers above"
+             )
+
+with volatility.container():
+
+
+    #
+    # predicted_volatility_line = alt.Chart(gdp).mark_line(color = 'blue').encode(
+    #     x=alt.X('date:T', axis = alt.Axis(format = '%Y', title = 'Date',tickCount="year"),
+    #     ),
+    #     y=alt.Y('predicted_volatility_modified_smoothed', axis = alt.Axis(title = 'Price ($/GWh)')).scale(zero = False),
+    #     # color = alt.datum('Predicted Volatility'),
+    #     color = alt.Color(
+    #         type="nominal",
+    #     legend=alt.Legend(title="Legend")
+    # ).scale(domain=['Predicted Volatility'], range=['blue'])
+    #
+    # ).interactive()
+    #
+    #
+    # base = alt.Chart(gdp).mark_line(color = 'red').encode(
+    #     x=alt.X('date:T', axis=alt.Axis(format='%Y', title='Date', tickCount="year"),
+    #             ),
+    #     y=alt.Y('smoothed_volatility', axis=alt.Axis(title='Price ($/GWh)')).scale(zero=False),
+    #     # color = alt.datum('Base Volatility')
+    #     color = alt.Color(
+    #         type="nominal",
+    #     legend=alt.Legend(title="Legend")
+    # ).scale(domain=['Base Volatility'], range=['red'])
+    # ).interactive()
+    #
+    #
+    # joined_chart_volatility = (predicted_volatility_line + base_volatility_line).properties(
+    #     title="Wholesale Electricity Volatility (Smoothed)"
+    # )
+    #
+    # st.altair_chart(joined_chart_volatility, use_container_width=True)
+
+
+    volatility_chart = alt.Chart(gdp).transform_fold(
+        fold=['Predicted Volatility', 'Base Volatility'],
+        as_=['predicted', 'base']
+        ).mark_line().encode(
+            x = alt.X('date:T', axis=alt.Axis(format='%Y', title='Date', tickCount="year")),
+            y=alt.Y('base:Q', axis=alt.Axis(title='Price ($/GWh)')).scale(zero=False),
+            color=alt.Color('predicted:N', legend=alt.Legend(title="Legend")).scale(
+                  domain=['Predicted Volatility', 'Base Volatility'],
+                  range=['orange', 'blue']
+
+
+            )
+
+        ).interactive().properties(
+        title="Wholesale Electricity Volatility (Smoothed)"
+    )
+
+    st.altair_chart(volatility_chart, use_container_width=True)
+
+    st.write("This shows the base volatility compared against a predicted Volatility calculated by the multipliers above")
+
+with price_impact.container():
+
+    threshold = gdp['Base Price'].quantile(0.95)
+    gdp['Price Spike'] = gdp['price_GWh'] > threshold
+    gdp['Predicted Price Spike'] = gdp['Predicted Price'] > threshold
+
+
+
+    # summary = pd.DataFrame({"Base": [gdp["Base Price"].mean(), gdp["Base Price"].std(), gdp["Price Spike"].mean()],
+    #               "Modified": [gdp["Predicted Price"].mean(), gdp["Predicted Price"].std(), gdp["Predicted Price Spike"].mean()],
+    #               },
+    #              index=["Average Price", "Volatility", "Spike Probability"])
+
+    summary = pd.DataFrame([[gdp["Base Price"].mean(),"Base Price", "Average"],
+                             [gdp["Base Price"].std(),"Base Price","Volatility"],
+                             [gdp["Price Spike"].mean(),"Base Price","Spike Probability"],
+                             [gdp["Predicted Price"].mean(),"Predicted Price","Average"],
+                             [gdp["Predicted Price"].std(),"Predicted Price","Volatility"],
+                             [gdp["Predicted Price Spike"].mean(),"Predicted Price","Spike Probability"]
+                            ],
+                             columns=["Value", "Scenario", "Data"])
+
+
+    # st.write(summary)
+
+    impact_chart = alt.Chart(summary).mark_bar().encode(
+        alt.X('Data', title=""),
+        alt.Y('Value', axis=alt.Axis(grid=False)),
+       color =  alt.Color('Scenario',).scale(
+                  domain=['Base Price', 'Predicted Price'],
+                  range=['blue', 'orange'])).properties(title="Price Impact of Predicted Scenario vs Base Values"
+)
+
+    st.altair_chart(impact_chart, use_container_width=True)
+
+    st.write("This shows the price impact under the base and predicted scenarios")
+
+
+
+
